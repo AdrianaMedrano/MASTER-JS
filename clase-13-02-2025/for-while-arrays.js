e14cf88257d02704bff6f477f4c8578c50c9cdc8
Adriana Medrano <adrianaxbxmxxg@gmail.com>
2025-03-04 01:04:09 -0600

    Agregue entregable de Funciones

--- clase-13-02-2025/for-while-arrays.js
+++ clase-13-02-2025/for-while-arrays.js
@@ -1,6 +1,6 @@
 //Combinanado while y for con arreglos 
 //Ejemplo 1: Recorrer un arreglo con while
-let frutas = ["MANZANA ", "UVA", "FRESA"]
+frutas = ["MANZANA ", "UVA", "FRESA"]
 let index = 0
 while (index < frutas.length){
     console.log("fruta", index, ";", frutas[index])
